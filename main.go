3c31effac200ca106fd6fe50800abfedd72295fe
seventh1976 <seventh1976@gmail.com>
2018-02-19 10:44:27 +0800

    adding func

--- main.go
+++ main.go
@@ -21,16 +21,60 @@ type Address struct {
 var people []Person
 
 // requests functions
-func GetPeople(w http.ResponseWriter, r *http.Request) {}
-func GetPerson(w http.ResponseWriter, r *http.Request) {}
-func CreatePerson(w http.ResponseWriter, r *http.Request) {}
-func DeletePerson(w http.ResponseWriter, r *http.Request) {}
+
+// list all people
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+  json.NewEncoder(w).Encode(people)
+}
+
+
+// show single people by id
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+  for _, item := range people {
+    if item.ID == params["id"] {
+      json.NewEncoder(w).Encode(item)
+      return
+    }
+  }
+  json.NewEncoder(w).Encode(&Person{})
+}
+
+
+// create a person
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+  var person Person
+  _ = json.NewDecoder(r.Body).Decode(&person)
+  person.ID = params["id"]
+  people = append(people, person)
+  json.NewEncoder(w).Encode(people)
+}
+
+
+// delete person
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+  for index, item := range people {
+    if item.ID == params["id"] {
+      people = append(people[:index], people[index+1:]...)
+      break
+    }
+    json.NewEncoder(w).Encode(people)
+  }
+}
+
 
 // main function
+
 func main() {
+
+  // dump records
   people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
   people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Y", State: "State Y"}})
   people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+
+  //routes
   router := mux.NewRouter()
   router.HandleFunc("/people", GetPeople).Methods("GET")
   router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
