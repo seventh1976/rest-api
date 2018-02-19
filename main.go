4c48ce86023d4d26ff0cb11524549a925be813c6
seventh1976 <seventh1976@gmail.com>
2018-02-19 09:50:10 +0800

    Adding structs & dump record

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-  //"encoding/json"
+  "encoding/json"
   "log"
   "net/http"
   "github.com/gorilla/mux"
 )
 
+type Person struct {
+  ID          string    `json:"id,omitempty"`
+  Firstname   string    `json:"firstname,omitempty"`
+  Lastname    string    `json:"lastname,omitempty"`
+  Address     *Address  `json:"address,omitempty"`
+}
+type Address struct {
+  City    string  `json:"city,omitempty"`
+  State   string  `json:"state,omitempty"`
+}
+
+var people []Person
 
 // requests functions
 func GetPeople(w http.ResponseWriter, r *http.Request) {}
@@ -16,6 +28,9 @@ func DeletePerson(w http.ResponseWriter, r *http.Request) {}
 
 // main function
 func main() {
+  people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+  people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Y", State: "State Y"}})
+  people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
   router := mux.NewRouter()
   router.HandleFunc("/people", GetPeople).Methods("GET")
   router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
